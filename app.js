9b610e2942b42459171081a18dc5a379b6ce1da3
Matias Acetosella <mati@MacBook-Pro-de-Matias.local>
2023-04-26 16:53:25 -0300

    fix

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const QRPortalWeb = require("@bot-whatsapp/portal");
 const BaileysProvider = require("@bot-whatsapp/provider/baileys");
 const MongoAdapter = require("@bot-whatsapp/database/mongo");
 
-const flowThanks = addKeyword("gracias").addAnswer("❤️");
+const flowThanks = addKeyword(["gracias"]).addAnswer("❤️");
 
 const getGreeting = () => {
     const currentDate = new Date();
@@ -35,40 +35,41 @@ const greetingAction = async (ctx, { flowDynamic }) => {
     return flowDynamic(`${greeting} ${name}, cómo estás? 😊`);
 };
 
-const productAnswers = (productName, productUrl) => {
-    return [
-        [
-            `Te envío el link a nuestra tienda con más info y precio de la ${productName}.`,
-            `👉🏼 ${productUrl}`,
-            "👉🏼 Podés llevar el modelo que más te guste \"en combo.\" Este incluye:",
-            "• Cuna • Colchón • Juego de sábanas • Almohadón de regalo 💖",
-        ],
-        [
-            "💳 Podés abonar en hasta en 3 cuotas sin interés con tarjetas de crédito bancarias.",
-            "💰 Efectivo: 20% OFF",
-            "💸 Transf/depósito:10% OFF",
-        ],
-        [
-            "⚡📦 Hacemos envíos a todo el país! Podés cotizar el mismo directamente desde nuestra página ingresando tu código postal."
-        ],
-        [
-            "📍Nos encontrás en 3 de Febrero 2962. Caseros, Buenos Aires."
-        ],
-        [
-            "Ante cualquier duda, estamos a tu disposición. 😊👑"
-        ]
-    ]
-};
-
-const configureFlowAnswers = (flow, productName, productUrl) => {
-    productAnswers(productName, productUrl).forEach((answer) => flow.addAnswer(answer));
-    flow.addAnswer(null, null, null, [flowThanks]);
-};
 
 const createProductFlow = ({ keyword, name, url }) => {
-    const flow = addKeyword(keyword).addAction(greetingAction);
-    configureFlowAnswers(flow, name, url);
-    return flow;
+    return addKeyword(keyword)
+        .addAction(greetingAction)
+        .addAnswer(
+            [
+                `Te envío el link a nuestra tienda con más info y precio de la ${name}.`,
+                `👉🏼 ${url}`,
+                "👉🏼 Podés llevar el modelo que más te guste \"en combo.\" Este incluye:",
+                "• Cuna • Colchón • Juego de sábanas • Almohadón de regalo 💖",
+            ]
+        )
+        .addAnswer(
+            [
+                "💳 Podés abonar en hasta en 3 cuotas sin interés con tarjetas de crédito bancarias.",
+                "💰 Efectivo: 20% OFF",
+                "💸 Transf/depósito:10% OFF",
+            ]
+        )
+        .addAnswer(
+            [
+                "⚡📦 Hacemos envíos a todo el país! Podés cotizar el mismo directamente desde nuestra página ingresando tu código postal."
+            ]
+        )
+        .addAnswer(
+            [
+                "📍Nos encontrás en 3 de Febrero 2962. Caseros, Buenos Aires."
+            ]
+        )
+        .addAnswer(
+            [
+                "Ante cualquier duda, estamos a tu disposición. 😊👑"
+            ]
+        )
+        .addAnswer(null, null, [flowThanks]);
 };
 
 const main = async () => {
